13693148472a27920aa56f0373402c599bebb797
peterkline1150 <peterkline1150@gmail.com>
2021-04-01 12:57:28 -0500

    Updating repository after changing a test method

--- Challenge1UnitTest/TestsForChallengeOne.cs
+++ Challenge1UnitTest/TestsForChallengeOne.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
-namespace Challenge1UnitTest
+namespace ChallengeOneUnitTest
 {
     [TestClass]
     public class TestsForChallengeOne
